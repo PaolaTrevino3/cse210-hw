ea9ca20f88153f4637f9586eb06f5f2876152020
PaolaTrevino3 <ptrevinochapa@byupathway.edu>
2026-05-10 16:36:06 -0500

    e3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
-class Program
+string playAgain = "yes";
+
+while (playAgain == "yes")
 {
-    static void Main(string[] args)
+    Random generator = new Random();
+    int magicNumber = generator.Next(1, 101);
+
+    int guess = -1;
+    int guessCount = 0;
+    Console.WriteLine("Welcome to the Magic Number Game!, i have chosen a number between 1 and 100, can you guess it?");
+
+    while (guess != magicNumber)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Enter your guess:");
+        guess = int.Parse(Console.ReadLine());
+        guessCount++;
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Too low! higher!");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Too high! lower!");
+        }
+        else
+        {
+            Console.WriteLine($"Congratulations! You've guessed the magic number {magicNumber} in {guessCount} guesses!");
+        }
+
+        Console.WriteLine("Do you want to play again? (yes/no)");
+        playAgain = Console.ReadLine().ToLower();
+
+        Console.WriteLine();
     }
 }
